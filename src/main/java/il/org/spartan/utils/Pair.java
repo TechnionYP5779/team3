fcf334bca469eaf3fd167f02ef5edc7258284ec9
DanielaDii <flower.dinka@gmail.com>
2018-11-02 16:59:35 +0200

    done #18

--- src/main/java/il/org/spartan/utils/Pair.java
+++ src/main/java/il/org/spartan/utils/Pair.java
@@ -1,6 +1,7 @@
 package il.org.spartan.utils;
 
 import org.jetbrains.annotations.*;
+import org.junit.*;
 
 public class Pair<First, Second> {
   @SuppressWarnings("unchecked") //
@@ -43,4 +44,45 @@ public class Pair<First, Second> {
   @Override @NotNull public String toString() {
     return "<" + first + "," + second + ">";
   }
+  
+  @SuppressWarnings("static-method") public static class TEST {
+    @Test public void basic_test() {
+      Pair<Integer,String>  p = new Pair<Integer,String>(1, "abc");
+      Pair<Integer,String>  p2 = new Pair<Integer,String>(1, "abc");
+      Pair<Integer,String>  p3 = new Pair<Integer,String>(3, "aaaa");
+      Pair<Integer,String>  p4 = new Pair<Integer,String>(1, "ac");
+      assert eq(p,p);
+      assert eq(p,p2);
+      assert !eq(p,p3);
+      assert !eq(p,p4);
+      assert p.equals(p);
+      assert p.equals(p2);
+      assert !p.equals(p3);
+      assert p.hashCode()!=p3.hashCode();
+      assert p.hashCode()!=p4.hashCode();
+      assert p.hashCode()==p2.hashCode();
+      assert p.hashCode()==hash(p);
+      assert p.toString().equals("<1,abc>");
+      Pair<String,Integer> n = p.newPair("X", 22);
+      assert !p.equals(n);
+    }
+    @Test public void Pairs_nulls_test() {
+      Pair<Integer,String>  p = new Pair<Integer,String>(1, "abc");
+      Pair<Integer,String>  p2 = new Pair<Integer,String>(1, "abc");
+      Pair<Integer,String>  p3 = new Pair<Integer,String>(3, "aaaa");
+      Pair<Integer,String>  p4 = new Pair<Integer,String>(1, "ac");
+      Pair<Integer,String> a[] = makePairs(3);
+      Pair<Integer,String> b[] = makePairs(3*2);
+      assert !eq(p,a[0]);
+      assert !p.equals(a[1]);
+      assert !eq(p,b[5]);
+      b[3]=p;
+      a[1]= new Pair<Integer,String>(1, "ac");
+      assert b[3].equals(p);
+      assert a[1].equals(p4);
+    }
+    
+    
+    
+  } 
 }
