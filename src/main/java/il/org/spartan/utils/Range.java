ab72d81267bea22adc3be42ac1a09079cecfd77d
DanielaDii <flower.dinka@gmail.com>
2018-11-02 16:59:35 +0200

    done #18

--- src/main/java/il/org/spartan/utils/Range.java
+++ src/main/java/il/org/spartan/utils/Range.java
@@ -3,6 +3,7 @@ package il.org.spartan.utils;
 import java.util.*;
 
 import org.jetbrains.annotations.*;
+import org.junit.*;
 
 /** An immutable integral range, representing all integers between
  * {@link #from}, up to, but not including, {@link #to}, i.e.,
@@ -93,5 +94,78 @@ public class Range {
 
   @Override public String toString() {
     return String.format("[%d, %d]", fluent.ly.box.it(from), fluent.ly.box.it(to));
+  }
+  
+  @SuppressWarnings("static-method") public static class TEST {
+    @Test public void test() {
+      Range zero = new Range(0,0);
+      Range zero_from1 = new Range(1,1);
+      Range r= new Range(1,10);
+      Range r2= new Range(1,11);
+      Range r3= new Range(-3,50);
+      Range r4= new Range(Integer.MIN_VALUE,Integer.MAX_VALUE);
+      Range bo= new Range(-5,2);
+      Range ao= new Range(9,11);
+      Range r_copy= new Range(r);
+      
+      assert zero.equals(zero);
+      assert !zero.equals(zero_from1);
+      assert !zero.equals(r4);
+      assert r.equals(r_copy);
+      
+      assert zero.hashCode()==zero.hashCode();
+      assert zero.hashCode()!=zero_from1.hashCode();
+      assert zero.hashCode()!=r4.hashCode();
+      assert r.hashCode()==r_copy.hashCode();
+
+      List<Range> l = new ArrayList<Range>();
+      assert r2.findIncludedIn(l)==null;
+      l.add(r);
+      l.add(r3);
+      l.add(r4);
+      assert r_copy.findIncludedIn(l)==r;
+      assert r_copy.equals(r_copy.findIncludedIn(l));
+      assert r3.findIncludedIn(l)==r3;
+      assert r4.findIncludedIn(l)==r4;
+      assert r2.findIncludedIn(l)==r3;
+      l.add(r2);
+      assert r2.findIncludedIn(l)!=r2;
+      assert r2.findIncludedIn(null)==null;
+      
+      r3.pruneIncluders(l);
+      assert l.size()==2;
+      zero.pruneIncluders(l);
+      assert l.size()==2;
+      l.remove(r);
+      r.pruneIncluders(l);
+      assert l.size()==0;
+      
+      assert r2.includedIn(r3);
+      assert !r2.includedIn(r);
+      assert !r.includedIn(zero);
+      assert !zero_from1.includedIn(zero);
+      assert r3.includedIn(r4);
+      
+      assert r.overlapping(r);
+      assert r.overlapping(r4);
+      assert !r4.overlapping(r);
+      assert r.overlapping(bo);
+      assert r.overlapping(ao);
+      
+      assert !r.isEmpty();
+      assert zero_from1.isEmpty();
+      
+      assert zero.size()==0;
+      assert r.size()==9;
+      assert r4.size()==Integer.MAX_VALUE-Integer.MIN_VALUE;
+      assert r3.merge(bo).size()==55;
+      assert r3.includedIn(r3.merge(bo));
+      assert bo.includedIn(r3.merge(bo));
+      assert !r4.includedIn(r3.merge(bo));
+      assert r3.merge(r4).size()==r4.size();
+
+      assert zero.toString().equals("[0, 0]");
+      assert r2.toString().equals("[1, 11]");
+    }
   }
 }
