fa6d5d7585cc85e8c95ae8bf219b6795bb4fad17
DanielaDii <flower.dinka@gmail.com>
2018-11-03 20:25:51 +0200

    done #41

--- src/main/java/fluent/ly/file.java
+++ src/main/java/fluent/ly/file.java
@@ -1,17 +1,16 @@
-package il.org.spartan.utils;
+package fluent.ly;
 
 import java.io.*;
 import java.nio.file.*;
 
 import org.jetbrains.annotations.*;
 
-import fluent.ly.*;
 
 /** File utils
  * @author Ori Marcovitch
  * @since Dec 4, 2016 */
-public enum file {
-  ;
+public class file {
+  
   private static void delete(final @NotNull String path) {
     if (file.exists(path))
       new File(path).delete();
@@ -29,7 +28,7 @@ public enum file {
   
 
   @NotNull public static String read(final @NotNull File f) throws IOException {
-    final @NotNull String ls = System.getProperty("line.separator");
+    @SuppressWarnings("null") final @NotNull String ls = System.getProperty("line.separator");
     System.err.println(ls.compareTo("\n"));
     final @NotNull StringBuilder $ = new StringBuilder();
     try (@NotNull BufferedReader reader = new BufferedReader(new FileReader(f))) {
