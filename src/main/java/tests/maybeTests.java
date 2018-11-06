7345d72946db3ca8058c64f1811ff323c1f913b4
ANNY <97anny79@gmail.com>
2018-11-06 21:23:12 +0200

    moving tests

---
+++ src/main/java/tests/maybeTests.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.*;
+
+import fluent.ly.*;
+
+@SuppressWarnings("static-method") 
+public class maybeTests {
+  @Test public void usecase0() {
+    azzert.isNull(maybe.no().get());
+  }
+
+  @Test public void usecase1() {
+    azzert.isNull(maybe.yes(null).get());
+  }
+
+  @Test public void usecase2() {
+    assert maybe.yes(new Object()).get() != null;
+  }
+}
