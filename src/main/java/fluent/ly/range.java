b1a641612ecd6046ebdae248e4b758a717878b05
DanielaDii <flower.dinka@gmail.com>
2018-11-09 18:01:40 +0200

    #61 box 3

--- src/main/java/fluent/ly/range.java
+++ src/main/java/fluent/ly/range.java
@@ -1,24 +1,35 @@
 package fluent.ly;
 
-import java.util.*;
-import java.util.stream.*;
+//import java.util.*;
+//import java.util.stream.*;
 
 
 public class range {
   
   //params
-
+  int from;
+//  boolean from_defined;
   //funcs & methods
   private range() {
-    
+//    from_defined=false;
   }
- 
+
+  private void set_from(int f) {
+    from=f;
+//    from_defined=true;
+  }
+  
   public static range from(int f) {
     range f_range=new range();
-   
+    f_range.set_from(f);
     return f_range;    
+  }
+  
+  public int from() {
+    return from;
   }
  
 }
+
 
 
--- src/main/java/tests/Tests_range_tdd.java
+++ src/main/java/tests/Tests_range_tdd.java
@@ -3,13 +3,18 @@ package tests;
 import org.junit.*;
 
 import fluent.ly.*;
-import il.org.spartan.utils.*;
+//import il.org.spartan.utils.*;
 
 public class Tests_range_tdd {
   
   @SuppressWarnings("static-method") @Test public void test() {
+    //box2
     range a = range.from(2);
     assert a!=null;
     assert a.getClass().equals(range.class);
+    
+    //box3
+    assert a.from()==2;
+    assert range.from(2).from()==2;
   }
 }
