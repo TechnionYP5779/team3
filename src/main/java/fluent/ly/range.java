474434f8a91eac1e36ba4b14e7786be323beb31b
DanielaDii <flower.dinka@gmail.com>
2018-11-09 17:54:19 +0200

    #61 box 2

---
+++ src/main/java/fluent/ly/range.java
@@ -0,0 +1,24 @@
+package fluent.ly;
+
+import java.util.*;
+import java.util.stream.*;
+
+
+public class range {
+  
+  //params
+
+  //funcs & methods
+  private range() {
+    
+  }
+ 
+  public static range from(int f) {
+    range f_range=new range();
+   
+    return f_range;    
+  }
+ 
+}
+
+
