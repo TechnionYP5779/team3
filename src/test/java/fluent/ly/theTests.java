f9ce18ceec76dfbe718318640921084e20eb4f0a
ANNY <97anny79@gmail.com>
2018-11-14 22:22:12 +0200

    removing supress boxing #90

--- src/test/java/fluent/ly/theTests.java
+++ src/test/java/fluent/ly/theTests.java
@@ -8,23 +8,23 @@ import org.junit.*;
 
 public class theTests {
   
-  @SuppressWarnings({ "boxing", "null", "unused", "static-method" }) @Test public void testos() {
+  @SuppressWarnings({ "null", "unused", "static-method" }) @Test public void testos() {
     azzert.assertTrue(the.nil()==null);
     azzert.assertTrue(the.nth("1","2").equals(" #1/2"));
     azzert.assertTrue(the.nth(1,2).equals(" #1/2"));
     azzert.assertTrue(the.nth(1,new ArrayList<Integer>()).equals(" #1/0"));
     azzert.assertTrue(the.penultimateOf(new ArrayList<Integer>())==null);
     List<Integer> li=new ArrayList<Integer>();
-    li.add(0,0);
-    li.add(1,1);
-    li.add(2,2);
-    li.add(3,3);
-    azzert.assertTrue(the.previous(1, li).equals(0));
-    azzert.assertTrue(the.previous(1, null)==null);
+    li.add(0,box.box(0));
+    li.add(1,box.box(1));
+    li.add(2,box.box(2));
+    li.add(3,box.box(3));
+    azzert.assertTrue(the.previous(box.box(1), li).equals(box.box(0)));
+    azzert.assertTrue(the.previous(box.box(1), null)==null);
     azzert.assertTrue(the.tailOf(li).size()==3);
     azzert.assertTrue(the.tailOf("123").equals("23"));
-    azzert.assertTrue(the.rest(1,li).size()==2);
-    azzert.assertTrue(the.headOf(li).equals(0));
+    azzert.assertTrue(the.rest(box.box(1),li).size()==2);
+    azzert.assertTrue(the.headOf(li).equals(box.box(0)));
     azzert.assertTrue(the.headOf(null)==null);
     azzert.assertTrue(the.headOf(new ArrayList<Integer>())==null);
     azzert.assertTrue(the.ith("123",0)=='1');
@@ -36,23 +36,23 @@ public class theTests {
     anArray = new Integer[10];
        
     // initialize first element
-    anArray[0] = 100;
+    anArray[0] = box.box(100);
     // initialize second element
-    anArray[1] = 200;
+    anArray[1] = box.box(200);
     // and so forth
-    anArray[2] = 300;
-    anArray[3] = 400;
-    anArray[4] = 500;
-    anArray[5] = 600;
-    anArray[6] = 700;
-    anArray[7] = 800;
-    anArray[8] = 900;
-    anArray[9] = 1000;
+    anArray[2] = box.box(300);
+    anArray[3] = box.box(400);
+    anArray[4] = box.box(500);
+    anArray[5] = box.box(600);
+    anArray[6] = box.box(700);
+    anArray[7] = box.box(800);
+    anArray[8] = box.box(900);
+    anArray[9] = box.box(1000);
 
-    azzert.assertTrue(the.lastOf(anArray)==1000);
+    azzert.assertTrue(the.lastOf(anArray).equals(box.box(1000)));
     azzert.assertTrue(the.tailOf(anArray).length==9);
     azzert.assertTrue(the.sqr(2)==4);
-    azzert.assertTrue(the.lastOf(li).equals(3));
+    azzert.assertTrue(the.lastOf(li).equals(box.box(3)));
     
     
     
