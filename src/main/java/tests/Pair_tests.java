af64edd0bb9eec75cfc77270ecb82b67ebff4b52
yogi <yogi@LAPTOP-CMTPT9BI>
2018-11-14 23:14:18 +0200

    cleanup

--- src/main/java/tests/Pair_tests.java
+++ src/main/java/tests/Pair_tests.java
@@ -6,8 +6,7 @@ import il.org.spartan.utils.*;
 
 @SuppressWarnings({ "static-access", "unused", "static-method", "boxing" }) public class Pair_tests {
   @Test public void basic_test() {
-    final Pair<Integer, String> p = new Pair<>(1, "abc"), p2 = new Pair<>(1, "abc"),
-        p3 = new Pair<>(3, "aaaa"), p4 = new Pair<>(1, "ac");
+    final Pair<Integer, String> p = new Pair<>(1, "abc"), p2 = new Pair<>(1, "abc"), p3 = new Pair<>(3, "aaaa"), p4 = new Pair<>(1, "ac");
     assert p.equals(p);
     assert p.equals(p2);
     assert !p.equals(p3);
@@ -22,8 +21,7 @@ import il.org.spartan.utils.*;
     new Pair<>(1, "abc");
     new Pair<>(3, "aaaa");
     new Pair<>(1, "abc");
-    final Pair<Integer, String> p = new Pair<>(1, "abc"), p4 = new Pair<>(1, "ac"), a[] = Pair.makePairs(3),
-        b[] = Pair.makePairs(6);
+    final Pair<Integer, String> p = new Pair<>(1, "abc"), p4 = new Pair<>(1, "ac"), a[] = Pair.makePairs(3), b[] = Pair.makePairs(6);
     assert !p.equals(a[1]);
     b[3] = p;
     a[1] = new Pair<>(1, "ac");
