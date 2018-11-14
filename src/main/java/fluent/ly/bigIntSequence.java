8aedc1ddf1b8ca8903e5a9a3c505c895d2f5215a
Eric <kiel.eric11@gmail.com>
2018-11-14 03:09:50 +0200

    Added bigIntegerSequence. Resolves #62

---
+++ src/main/java/fluent/ly/bigIntSequence.java
@@ -0,0 +1,60 @@
+package fluent.ly;
+
+import java.math.*;
+import java.util.*;
+
+public class bigIntSequence{
+  
+  public static class bla implements Iterable<BigInteger>, Iterator<BigInteger>{
+    private BigInteger current;
+    private BigInteger step;
+    private BigInteger end;
+  
+    public bla(BigInteger start, BigInteger step, BigInteger end) {
+      this.current = start;
+      this.step = step;
+      this.end = end;
+    }
+    
+    public bla from(BigInteger start) {
+      this.current = start;
+      return this;
+    }
+    
+    public bla step(BigInteger step) {
+      this.step = step;
+      return this;
+    }
+    public bla to(BigInteger end) {
+      this.end = end;
+      return this;
+    }
+    
+    @Override
+    public Iterator<BigInteger> iterator() {
+      return new bla(current, step, end);
+    }
+    
+    @Override public BigInteger next() {
+      BigInteger tmp = current;
+      current = current.add(step);
+      return tmp;
+    }
+    
+    @Override public boolean hasNext() {
+      return (end == null) ? true : 
+        (current.compareTo(end) >= 0) ? false : true;
+    }
+  }
+
+  public static bla from(BigInteger start) {
+    return new bla(start, BigInteger.ZERO, null);
+  }
+  
+  public static bla step(BigInteger step) {
+    return new bla(BigInteger.ZERO, step, null);
+  }
+  public static bla to(BigInteger end) {
+    return new bla(BigInteger.ZERO, BigInteger.ZERO, end);
+  }
+}
