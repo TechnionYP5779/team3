4523740a8b10cfd6467069103b64779f06927e45
ANNY <97anny79@gmail.com>
2018-11-15 15:17:52 +0200

    fix bug

--- src/test/java/il/org/spartan/utils/OuterTest.java
+++ src/test/java/il/org/spartan/utils/OuterTest.java
@@ -5,11 +5,11 @@ import org.junit.*;
 import fluent.ly.*;
 
 public class OuterTest {
-  @Test @SuppressWarnings({ "unchecked", "boxing", "null", "static-method" }) public void outerAdds() {
+  @Test @SuppressWarnings({ "unchecked", "null", "static-method" }) public void outerAdds() {
     @SuppressWarnings({ "rawtypes" }) final Outer o = new Outer(10);
     @SuppressWarnings({ "rawtypes" }) final Outer o1 = new Outer(10);
     azzert.assertTrue(o.equals(o1));
     o.set(11);
-    azzert.assertTrue((int) o.get() == 11);
+    azzert.assertTrue(((Integer) o.get()).equals(box.box(11)));
   }
 }
--- src/test/java/il/org/spartan/utils/Pair_tests.java
+++ src/test/java/il/org/spartan/utils/Pair_tests.java
@@ -5,10 +5,10 @@ import org.junit.*;
 import fluent.ly.*;
 import il.org.spartan.utils.*;
 
-@SuppressWarnings({ "unused", "static-method", "boxing" }) public class Pair_tests {
+@SuppressWarnings({ "unused", "static-method" }) public class Pair_tests {
   @Test public void basic_test() {
-    final Pair<Integer, String> p = new Pair<>(1, "abc"), p2 = new Pair<>(1, "abc"),
-        p3 = new Pair<>(3, "aaaa"), p4 = new Pair<>(1, "ac");
+    final Pair<Integer, String> p = new Pair<>(box.box(1), "abc"), p2 = new Pair<>(box.box(1), "abc"),
+        p3 = new Pair<>(box.box(3), "aaaa"), p4 = new Pair<>(box.box(1), "ac");
     azzert.assertTrue( p.equals(p));
     azzert.assertTrue( p.equals(p2));
     azzert.assertTrue( !p.equals(p3));
@@ -16,18 +16,18 @@ import il.org.spartan.utils.*;
     azzert.assertTrue( p.hashCode() != p4.hashCode());
     azzert.assertTrue( p.hashCode() == p2.hashCode());
     azzert.assertTrue( "<1,abc>".equals(p + ""));
-    azzert.assertTrue( !p.equals(Pair.newPair("X", 22)));
+    azzert.assertTrue( !p.equals(Pair.newPair("X", box.box(22))));
   }
 
   @Test public void Pairs_nulls_test() {
-    new Pair<>(1, "abc");
-    new Pair<>(3, "aaaa");
-    new Pair<>(1, "abc");
-    final Pair<Integer, String> p = new Pair<>(1, "abc"), p4 = new Pair<>(1, "ac"), a[] = Pair.makePairs(3),
+    new Pair<>(box.box(1), "abc");
+    new Pair<>(box.box(3), "aaaa");
+    new Pair<>(box.box(1), "abc");
+    final Pair<Integer, String> p = new Pair<>(box.box(1), "abc"), p4 = new Pair<>(box.box(1), "ac"), a[] = Pair.makePairs(3),
         b[] = Pair.makePairs(6);
     azzert.assertTrue( !p.equals(a[1]));
     b[3] = p;
-    a[1] = new Pair<>(1, "ac");
+    a[1] = new Pair<>(box.box(1), "ac");
     azzert.assertTrue( b[3].equals(p));
     azzert.assertTrue( a[1].equals(p4));
   }
