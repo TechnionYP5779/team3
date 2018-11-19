a10f81d157df398cf5f25497ad8557d5aa69ba3e
ANNY <97anny79@gmail.com>
2018-11-19 19:15:20 +0200

    boxing things #105

--- src/test/java/fluent/ly/UnboxTests.java
+++ src/test/java/fluent/ly/UnboxTests.java
@@ -1,5 +1,7 @@
 package fluent.ly;
 
+import static fluent.ly.azzert.*;
+
 import java.util.*;
 
 import org.junit.*;
@@ -18,22 +20,22 @@ import org.junit.*;
     lst.add(5);
     lst.add(6);
     int[] res = unbox.it(lst);
-    azzert.assertEquals(res.length, arr.length);
+    azzert.that(res.length, is(arr.length));
     int j = 0;
     for (final int ¢ : arr)
-      azzert.assertEquals(¢, res[j++]);
+      azzert.that(¢, is(res[j++]));
     final Integer[] arrInt = { 1, 2, 3, 4, 5, 6 };
     res = unbox.it(arrInt);
     azzert.assertEquals(res.length, arr.length);
     j = 0;
     for (final int ¢ : arr)
       azzert.assertEquals(¢, res[j++]);
-    azzert.assertEquals(unbox.unbox(Integer.valueOf(5)), 5);
+    azzert.that(unbox.unbox(box.box(5)), is(5));
     res = unbox.unbox(arrInt);
-    azzert.assertEquals(res.length, arr.length);
+    azzert.that(res.length, is(arr.length));
     j = 0;
     for (final int ¢ : arr)
-      azzert.assertEquals(¢, res[j++]);
+      azzert.that(¢, is(res[j++]));
   }
 
   @Test public void testShorts() {
@@ -95,10 +97,10 @@ import org.junit.*;
   @Test public void testBools() {
     final boolean[] arr = { true, false, true, true, false, false };
     final Boolean[] arrBool = { true, false, true, true, false, false };
-    azzert.assertEquals(unbox.unbox(Boolean.TRUE), true);
-    azzert.assertEquals(unbox.unbox(Boolean.FALSE), false);
+    assert unbox.unbox(Boolean.TRUE);
+    assert !unbox.unbox(Boolean.FALSE);
     final boolean[] res = unbox.unbox(arrBool);
-    azzert.assertEquals(res.length, arr.length);
+    azzert.that(res.length, is(arr.length));
     int j = 0;
     for (final boolean i : arr)
       azzert.assertEquals(i, res[j++]);
