460656efe56b581d5df18d1e1d99cb07d0fa66f7
yanivbaldi <yaniv-b@campus.technion.ac.il>
2018-11-20 20:01:24 +0200

    issue #35 update

--- src/test/java/il/org/spartan/tables/TableTest.java
+++ src/test/java/il/org/spartan/tables/TableTest.java
@@ -1,11 +1,20 @@
 package il.org.spartan.tables;
 
+
+
 import org.junit.*;
 
-@SuppressWarnings({"static-method"}) public class TableTest {
+
+
+@SuppressWarnings({"static-method","resource"}) public class TableTest {
   
-  @Test public void aa() {
-    
+  @Test public void Table1() {
+    Integer n = new Integer(11245);
+    Table t = new Table(n);
+    Table t1= new Table("",TableRenderer.builtin.values()); 
+    assert(t.equals(t1));
   }
-  
+  @Test public void Table2() {
+    //Table t = new Table("Integer","C:\\" );
+  }
 }
