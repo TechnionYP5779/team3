837023a872c5171ce891c099691c4ea897e91731
yanivbaldi <yaniv-b@campus.technion.ac.il>
2018-11-20 20:16:37 +0200

    renamed files

---
+++ src/test/java/fluent/ly/YesTest.java
@@ -0,0 +1,18 @@
+package fluent.ly;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class yesTest {
+  @Test public void yestest() {
+    final Object o1 = new Object();
+    final Object o2 = new Object();
+    final Runnable f = () -> {
+      // test
+    };
+    azzert.assertTrue(yes.forgetting(o1, o2));
+    azzert.assertTrue(yes.ignoring(true));
+    azzert.assertTrue(yes.ignoring(1.0));
+    azzert.assertTrue(yes.ignoring(5L));
+    azzert.assertTrue(yes.forgetting(f));
+  }
+}
