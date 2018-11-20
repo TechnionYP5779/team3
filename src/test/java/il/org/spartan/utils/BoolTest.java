354fa79f58fca65de4b58ebb4a311a12a7b1f81c
ANNY <97anny79@gmail.com>
2018-11-20 21:30:49 +0200

    added Bool tests #15

--- src/test/java/il/org/spartan/utils/BoolTest.java
+++ src/test/java/il/org/spartan/utils/BoolTest.java
@@ -1,5 +1,8 @@
 package il.org.spartan.utils;
 
+import static fluent.ly.unbox.*;
+import static il.org.spartan.Utils.*;
+
 import org.jetbrains.annotations.*;
 import org.junit.*;
 
@@ -11,5 +14,11 @@ import org.junit.*;
     assert !b.get();
     b.set(true);
     assert b.get();
+    b.set(false);
+    assert !unbox(cantBeNull(b.inner()));
+    final @NotNull Bool b2 = new Bool();
+    b2.set();
+    assert b2.get();
+    assert unbox(cantBeNull(Bool.valueOf(true).inner()));
   }
 }
