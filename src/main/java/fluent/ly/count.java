dc4c941730501678f2d0da45953b086ee2c37350
ANNY <97anny79@gmail.com>
2018-11-20 23:00:10 +0200

    fix bug

--- src/main/java/fluent/ly/count.java
+++ src/main/java/fluent/ly/count.java
@@ -3,8 +3,11 @@ package fluent.ly;
 public interface count {
   static <T> int of(final Iterable<T> ts) {
     int $ = 0;
-    for (final T __ : ts)
+    for (final T __ : ts) {
       forget.it( __ );
-      return ++$;
+      ++$;
+    }
+    return $;
   }
+  
 }
