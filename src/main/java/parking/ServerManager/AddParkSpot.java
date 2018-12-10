4ded790ade400ba7bc8ef4d7e50a288b61a7aa54
ANNY <97anny79@gmail.com>
2018-12-10 15:51:18 +0200

    working on #162

--- src/main/java/parking/ServerManager/AddParkSpot.java
+++ src/main/java/parking/ServerManager/AddParkSpot.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet Tutorial - Servlet Example
  */
 @WebServlet(
-    description = "Park4You Login", 
+    description = "Park4You Add Park Spot", 
     urlPatterns = { "/AddParkSpot" })
 public class AddParkSpot extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -32,6 +32,20 @@ public class AddParkSpot extends HttpServlet {
 
   
   @Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    //TODO
+  //get parameters
+    String city = request.getParameter("city");
+    String street = request.getParameter("street");
+    String BuildNum = request.getParameter("BuildNum");
+    String date = request.getParameter("date");
+    String FromHour = request.getParameter("FromHour");
+    String ToHour = request.getParameter("ToHour");
+    
+    //TODO save to DB
+    //TODO redirect to correct page.
+    
+    try(PrintWriter out = response.getWriter();){
+      out.println(HTML_START + "<h2>Hi There!</h2><br/><h3>city="+ city +"</h3><h3>street="+ street +"</h3><h3>building="+BuildNum + "</h3><h3>date=" +date+"</h3><h3>from=" + FromHour + "</h3><h3>to=" + ToHour + "</h3>"+HTML_END);
+    }
+    
   }
 }
