7f6e80a7b26cb79d71ed44ee931f2f1c3f1dda9d
Rani Jammal <rani.j@campus.technion.ac.il>
2018-12-21 03:53:00 -0800

    -Working on integrating auth0 loigin authentication, added proper Servlets AuthOFFilter that checks if we are already logged in LoginServlet responsible for logging in CallbackServlet responsible for redirecting after logging in LogoutServlet used for logging out HomeServlet used to redirect to home after logging in -changed index.html to use auth0 login now instead of the old system -changed web.xml to point to another auth0 account that i created since i didnt have the password for last one

---
+++ src/main/java/parking/ServerManager/LoginServlet.java
@@ -0,0 +1,74 @@
+
+package parking.ServerManager;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import com.auth0.AuthenticationController;
+import javax.servlet.ServletConfig;
+import java.io.UnsupportedEncodingException;
+/**
+ * Servlet Tutorial - Servlet Example
+ */
+@WebServlet(
+    description = "Park4You Login", 
+    urlPatterns = { "/Login" })
+public class LoginServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  public static final String HTML_START="<html><body>";
+  public static final String HTML_END="</body></html>";
+  private AuthenticationController authenticationController;
+  private String domain;
+  //we can create DB connection resource here and set it to Servlet context
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+      super.init(config);
+      domain = config.getServletContext().getInitParameter("com.auth0.domain");
+      try {
+          authenticationController = AuthenticationControllerProvider.getInstance(config);
+      } catch (UnsupportedEncodingException e) {
+          throw new ServletException("Couldn't create the AuthenticationController instance. Check the configuration.", e);
+      }
+  }
+
+  @Override
+  protected void doGet(final HttpServletRequest req, final HttpServletResponse res) throws ServletException, IOException {
+      String redirectUri = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/callback";
+
+      String authorizeUrl = authenticationController.buildAuthorizeUrl(req, redirectUri)
+              .withAudience(String.format("https://%s/userinfo", domain))
+              .build();
+      res.sendRedirect(authorizeUrl);
+  }
+
+  
+  @Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String redirectUri = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/callback";
+
+    String authorizeUrl = authenticationController.buildAuthorizeUrl(request, redirectUri)
+            .withAudience(String.format("https://%s/userinfo", domain))
+            .build();
+    response.sendRedirect(authorizeUrl);
+/*
+    //get parameters
+    String userName = request.getParameter("userName");
+    String password = request.getParameter("pwd");
+    //Some logs to understand this :)
+    
+    log("-------------------------------");
+    log("user is " + userName);
+    log("password is " + password);
+    log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    
+    
+    //TODO check username and password
+    //TODO maybe change to user id?
+    Cookie user = new Cookie("user", userName);
+    //TODO set expire right
+    user.setMaxAge(60*60*24);
+    response.addCookie( user );
+    response.sendRedirect("homePage.html");
+    return; */
+  }
+}
