46b68a4c26c95b8e434aa04d4c7ae821d17c293a
DanielaDii <flower.dinka@gmail.com>
2018-12-22 19:26:43 +0200

    not bla

--- src/main/java/parking/db/Parking.java
+++ src/main/java/parking/db/Parking.java
@@ -22,25 +22,17 @@ public class Parking {
     this.Price=Price;
   }
   
-  public String getAddress() {
-    return address;
-  }
+  
 
   public void setAddress(String address) {
     this.address = address;
   }
 
-  public double getLat() {
-    return lat;
-  }
 
   public void setLat(double lat) {
     this.lat = lat;
   }
 
-  public double getLon() {
-    return lon;
-  }
 
   public void setLon(double lon) {
     this.lon = lon;
